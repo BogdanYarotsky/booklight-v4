466aff014f57edafd47154bf2b412e80c5336ee4
BogdanYarotsky <byarotsky@gmail.com>
2023-04-20 20:53:29 +0200

    wtf is going on

--- go-api/main.go
+++ go-api/main.go
@@ -1,21 +1,50 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
+type Response struct {
+	Query string `json:"query"`
+}
+
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		if r.Method == "GET" {
-			fmt.Fprintf(w, "Hello, world!")
-		} else {
-			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+	endpoint := "localhost:50051"
+	creds := grpc.WithTransportCredentials(insecure.NewCredentials())
+	conn, err := grpc.Dial(endpoint, creds)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+
+	http.HandleFunc("/api/search", func(w http.ResponseWriter, r *http.Request) {
+				query := r.URL.Query().Get("q")
+
+		if query == "" {
+			http.Error(w, "missing query parameter 'q'", http.StatusBadRequest)
+			return
 		}
+
+		// results := searchService.Search(query)
+
+		response := Response{Query: query}
+		// add search results to the response
+		// ...
+		jsonBytes, err := json.Marshal(response)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		fmt.Fprint(w, string(jsonBytes))
 	})
 
-	fmt.Println("Listening on :8080...")
-	if err := http.ListenAndServe(":8080", nil); err != nil {
-		panic(err)
-	}
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
--- go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/BogdanYarotsky/booklight-v4
-
-go 1.20
